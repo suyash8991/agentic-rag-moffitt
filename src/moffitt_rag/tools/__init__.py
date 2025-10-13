ec27b1acdb613fc50f7f8a31fdfd9680cd2b08a5
Suyash Sreekumar <suyash.sreekumar@gmail.com>
2025-10-13 15:52:28 -0700

    Implement ProgramFilterTool for program-based filtering

--- src/moffitt_rag/tools/__init__.py
+++ src/moffitt_rag/tools/__init__.py
@@ -4,5 +4,6 @@ Tools for the agent to interact with researcher data.
 
 from .researcher_search import ResearcherSearchTool
 from .department_filter import DepartmentFilterTool
+from .program_filter import ProgramFilterTool
 
-__all__ = ["ResearcherSearchTool", "DepartmentFilterTool"]
+__all__ = ["ResearcherSearchTool", "DepartmentFilterTool", "ProgramFilterTool"]
